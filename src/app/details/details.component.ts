c9263bfbe902aa84dfba804736f735678f5c8ade
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-04-22 22:57:52 +0530

    Routing in Angular done

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { inject } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details works! {{housingLocationId}}
+    </p>
+  `,
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params['id'])
+  }
+
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Housinglocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo">
     <h2 class="listing-heading">{{housingLocation.name}}</h2>
     <p  class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
+    <a [routerLink]="['/details',housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrl: './housing-location.component.css'
