e5c1501c644d9b24869ef8f2ed5357c90874bc13
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-04-22 22:19:09 +0530

    Homes listed and looks good

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housing-location';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo">
+    <h2 class="listing-heading">{{housingLocation.name}}</h2>
+    <p  class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
+  </section>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!:Housinglocation;
+}
